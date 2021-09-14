1ff2bdc5775ec3b6cb0fae8b108437663f38e1c2
Mitchell <mwhyuen@gmail.com>
2021-09-14 10:06:05 +0200

    feat addToCartHandler in ProductItem

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,17 +1,35 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCTS = [
+  {
+    id: 1,
+    title: "Test",
+    price: 6,
+    description: "This is a first product - amazing!",
+  },
+  {
+    id: 2,
+    title: "Test2",
+    price: 8,
+    description: "This is a second product - amazing!",
+  },
+];
 
 const Products = (props) => {
+  const products = DUMMY_PRODUCTS.map((product) => (
+    <ProductItem
+      key={product.id}
+      title={product.title}
+      price={product.price}
+      description={product.description}
+      id={product.id}
+    />
+  ));
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
-      <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
-      </ul>
+      <ul>{products}</ul>
     </section>
   );
 };
